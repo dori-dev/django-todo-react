1d87c8c9f4d38fe8cbdbce032afa7e0f8672fc88
mohammad <mr.dori.dev@gmail.com>
2022-03-19 11:07:13 +0330

    register todo model to django admin

--- backend/todo/admin.py
+++ backend/todo/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Todo
 
-# Register your models here.
+
+class TodoAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'completed')
+
+
+admin.site.register(Todo, TodoAdmin)
