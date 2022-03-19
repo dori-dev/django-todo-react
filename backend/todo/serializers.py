3bdbc33c9ca4f74d7e8662d4b8b521b7fe934798
mohammad <mr.dori.dev@gmail.com>
2022-03-19 11:22:48 +0330

    use django rest for api

---
+++ backend/todo/serializers.py
@@ -0,0 +1,16 @@
+"""serializers
+"""
+
+from rest_framework import serializers
+from .models import Todo
+
+
+class TodoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Todo
+        fields = (
+            'id',
+            'title',
+            'description',
+            'completed',
+        )
