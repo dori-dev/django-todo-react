87b9385b43ffc32aec4c794ae51f6a4480cf918b
mohammad <mr.dori.dev@gmail.com>
2022-03-19 11:04:03 +0330

    add todo model

--- backend/todo/models.py
+++ backend/todo/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Todo(models.Model):
+    title = models.CharField(max_length=120)
+    description = models.TextField()
+    completed = models.BooleanField(default=False)
+
+    def __str__(self) -> str:
+        return self.title
